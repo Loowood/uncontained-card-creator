7a4b2d7af09d79040c10a606e5f83b7b0ec56b62
Loowood <lorisg01@yahoo.fr>
2020-08-13 18:05:21 +0200

    First Tag + Name + class Name

--- public/index.js
+++ public/index.js
@@ -2,6 +2,11 @@ var canvas;
 var ctx;
 var objClass;
 var backgroundType;
+var cardClass;
+var cardClassImg = false;
+var cardName;
+var cardFlavor;
+var firstTag;
 
 window.onload =  function () {
 	canvas = document.getElementById('myCanvas');
@@ -12,8 +17,29 @@ function clearing() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function nameCardChange() {
+function nameChange() {
 	cardName = event.target.value;
+	
+	redraw();
+}
+
+function flavorChange() {
+	cardFlavor = event.target.value;
+	
+	redraw();
+}
+
+function classChange() {
+	cardClass = event.target.innerText.toUpperCase();
+	
+	cardClassImg = (cardClass == "ANOMALY/CHARACTER");
+	
+	redraw();
+}
+
+function firstTagChange() {
+	firstTag = event.target.innerText.toUpperCase();
+	// firstTagPath = './icons/' + firstTag + '.png';
 	
 	redraw();
 }
@@ -27,8 +53,6 @@ function backgrounding () {
 
 function redraw() {
 	clearing();
-	ctx.fillStyle = 'black';
-	ctx.font = '48px Verdana';
 	
 	if (backgroundType) {
 		var bg_ToDraw = new Image(750, 1050);
@@ -40,15 +64,50 @@ function redraw() {
 			ctx.drawImage(bg_ToDraw,0,0);
 			drawText();
 		});
-	} else {
-		drawText();
 	}
+	if (cardClassImg) {
+		var class_ToDraw = new Image(436, 109);
+		class_ToDraw.src = './misc/AnomalyCharacterContainer.png';
+		console.log(class_ToDraw);
+		
+		class_ToDraw.addEventListener("load", function () {
+			console.log("Loaded !");
+			ctx.drawImage(class_ToDraw, 20, 103);
+			drawText();
+		});
+	}
+	if (firstTag) {
+		var firstTag_ToDraw = new Image(70, 70);
+		firstTag_ToDraw.src = './icons/' + firstTag + '.png';
+		console.log(firstTag_ToDraw);
+		
+		firstTag_ToDraw.addEventListener("load", function() {
+			console.log("Tag Loaded!");
+			ctx.drawImage(firstTag_ToDraw, 640, 175);
+			drawText();
+		});
+	}
+	
+	drawText();
 }
 
 function drawText() {
-	if (cardName) {
+	if (cardFlavor) {
+		ctx.fillStyle = 'black';
 		ctx.font = "48px ITC Bauhaus";
 		ctx.textAlign = "center";
-		ctx.fillText(cardName, 750/2, 70);
+		ctx.fillText(cardFlavor, 750/2, 70);
+	}
+	if (!cardClassImg && cardClass) {
+		ctx.fillStyle = 'black';
+		ctx.font = "40px ITC Bauhaus";
+		ctx.textAlign = "center";
+		ctx.fillText(cardClass, 150, 185);
+	}
+	if (cardName) {
+		ctx.fillStyle = 'black';
+		ctx.font = "40px ITC Bauhaus";
+		ctx.textAlign = "center";
+		ctx.fillText(cardName, 525, 645);
 	}
 }
